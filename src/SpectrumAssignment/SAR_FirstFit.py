77b3742e30a3c0b9be109c869f457874ec4715a6
Matheus Lôbo <matheusls@live.com>
2024-01-26 18:50:50 -0300

    Improvement SAR speed

--- src/SpectrumAssignment/SAR_FirstFit.py
+++ src/SpectrumAssignment/SAR_FirstFit.py
@@ -12,14 +12,14 @@ import numpy as np
 
 #             if not np.any(availability_vector_uplink[slot : slot + demands]):
 #                 return route, list(range(slot, slot + demands))
-    
+
 #     return None, []
 
 
 
 def find_slots(routes: list, demands: int) -> np.array:
     """ Encontra os slots disponíveis para alocar a demanda usando o método SAR. O método SAR aloca a demanda na rota que possua slots disponíveis no menor índice possível seguindo a ordem em que as rotas aparecem na lista de rotas.
-    
+
         Parâmetros:
             -routes (list): Lista de rotas.
             -demands (int): Número de slots da demanda.
@@ -31,24 +31,73 @@ def find_slots(routes: list, demands: int) -> np.array:
 
     number_of_slots = routes[0]._number_of_slots
 
+    availability_vector_uplink_by_route = []
+
     # Percorre os slots de menor índice até o último slot possível para alocar a demanda
     for slot in range(number_of_slots - demands):
         # Percorre cada rota na ordem em que aparece na lista de rotas
-        for route in routes:
+        for index, route in enumerate(routes):
 
-            uplinks = route.get_uplinks()
+            if index == len(availability_vector_uplink_by_route):
 
-            # Constroi o vetor de disponibilidade de slots para o uplink
-            availability_vector_uplink = uplinks[0]
-            for link in range(1, len(uplinks)):
-                availability_vector_uplink = np.logical_or(availability_vector_uplink, uplinks[link])
+                uplinks = route.get_uplinks()
+
+                #Constroi o vetor de disponibilidade de slots para o uplink
+                availability_vector_uplink = uplinks[0]
+                for link in range(1, len(uplinks)):
+                    availability_vector_uplink = np.logical_or(availability_vector_uplink, uplinks[link])
+
+                availability_vector_uplink_by_route.append(availability_vector_uplink)
+
+            #availability_vector_uplink = np.sum(route.get_uplinks(), axis=0, dtype=bool)
 
             is_available = True
             for j in range(demands):
-                if availability_vector_uplink[slot + j]:
+                if availability_vector_uplink_by_route[index][slot + j]:
                     is_available = False
                     break
             if is_available:
                 return route, list(range(slot, slot + demands))
-        
+
     return None, []
+
+
+# def find_slots(routes: list, demands: int) -> np.array:
+#     """ Encontra os slots disponíveis para alocar a demanda usando o método SAR. O método SAR aloca a demanda na rota que possua slots disponíveis no menor índice possível seguindo a ordem em que as rotas aparecem na lista de rotas.
+
+#         Parâmetros:
+#             -routes (list): Lista de rotas.
+#             -demands (int): Número de slots da demanda.
+
+#         Retorno:
+#             -route (Route): Rota onde foi possível alocar a demanda.
+#             -slots (list[int]): Lista de slots alocados.
+#     """
+
+#     number_of_slots = routes[0]._number_of_slots
+
+#     availability_vector_uplink_by_route = []
+
+#     # Percorre os slots de menor índice até o último slot possível para alocar a demanda
+#     for slot in range(number_of_slots - demands):
+#         # Percorre cada rota na ordem em que aparece na lista de rotas
+#         for route in routes:
+
+#             # uplinks = route.get_uplinks()
+
+#             # #Constroi o vetor de disponibilidade de slots para o uplink
+#             # availability_vector_uplink = uplinks[0]
+#             # for link in range(1, len(uplinks)):
+#             #     availability_vector_uplink = np.logical_or(availability_vector_uplink, uplinks[link])
+
+#             availability_vector_uplink = np.sum(route.get_uplinks(), axis=0, dtype=bool)
+
+#             is_available = True
+#             for j in range(demands):
+#                 if availability_vector_uplink[slot + j]:
+#                     is_available = False
+#                     break
+#             if is_available:
+#                 return route, list(range(slot, slot + demands))
+
+#     return None, []
